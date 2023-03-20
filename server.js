09078c521d4173f49d6d578fc1fe7a8aae56b364
Amir <amirrafiq24@gmail.com>
2023-03-20 02:30:01 +0000

    cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Menu = require("./model/menu");
+const cors = require("cors");
 require("dotenv/config");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
 const menuRoutes = require("./routes/menu");
 
